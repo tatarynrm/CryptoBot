73d117ff8b02310af1c76c4268be7dcc2654e186
tatarynrm <tatarynrm@gmail.com>
2022-07-13 16:18:32 +0300

    memory

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const owner = '@web_developer_Ukraine';
 const v8 = require('v8')
 const totalHeapSize = v8.getHeapStatistics().total_available_size
 let totalHeapSizeInGB = (totalHeapSize / 1024 / 1024 / 1024).toFixed(2)
-console.log(`Total Heap now : ${totalHeapSize}, (GB ~ ${totalHeapSizeInGB})`);
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) =>
