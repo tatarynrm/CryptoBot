2b86c61c4fa0ca47fb9d2a8ad7f2ec7a18f048c1
tatarynrm <tatarynrm@gmail.com>
2022-07-13 16:18:19 +0300

    memory size

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const date = require('date-and-time');
 const data = require('./cryptos.json')
 const data2 = require('./cryptos2.json')
 const owner = '@web_developer_Ukraine';
+
+const v8 = require('v8')
+const totalHeapSize = v8.getHeapStatistics().total_available_size
+let totalHeapSizeInGB = (totalHeapSize / 1024 / 1024 / 1024).toFixed(2)
+console.log(`Total Heap now : ${totalHeapSize}, (GB ~ ${totalHeapSizeInGB})`);
+
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) =>
     ctx.reply(`
